864be6ee14c1f08a13a064a8aae93199968584ee
Владимир <vladsidorenko@ozon.ru>
2025-01-13 16:33:33 +0300

    upd

--- lab_2_table.py
+++ lab_2_table.py
@@ -1,3 +1,4 @@
+import pandas as pd
 from prettytable import PrettyTable
 import numpy as np
 from scipy.integrate import quad
@@ -10,28 +11,33 @@ def f(x):
 a, b = 1, 4
 
 # Число точек для аппроксимации
-N = int(input("Введите число N: "))
+nums = list(map(int, input("Введите 3 числа N: ").split()))
 
-# Вычисление математического ожидания
-Mx, _ = quad(lambda x: x * f(x), a, b)
-
-# Вычисление дисперсии
-Dx, _ = quad(lambda x: (x - Mx)**2 * f(x), a, b)
-
-#вычисление погрешности
-m = np.mean(np.random.uniform(a, b, size=N))
-g = np.var(np.random.uniform(a, b, size=N))
-
-# Вычисление погрешностей
-D1 = abs(Mx - m)
-D2 = abs(Dx - g)
-
-# Создание таблицы
 mytable = PrettyTable()
-mytable.field_names = ["N", "Mx", "m", "D1", "Dx", "g", "D2"]
+mytable.field_names = ["N", "Mx", "m", "D1 - погрешность Mx", "Dx", "g", "D2 - погрешность Dx"]
 
-# Заполнение таблицы
-mytable.add_row([N, round(Mx, 4), round(m, 4), round(D1, 4), round(Dx, 4), round(g, 4), round(D2, 4)])
+def calculate(N):
+    # Вычисление математического ожидания
+    Mx, _ = quad(lambda x: x * f(x), a, b)
+
+    # Вычисление дисперсии
+    Dx, _ = quad(lambda x: (x - Mx)**2 * f(x), a, b)
+
+    #вычисление погрешности
+    m = np.mean(np.random.uniform(a, b, size=N))
+    g = np.var(np.random.uniform(a, b, size=N))
+
+    # Вычисление погрешностей
+    D1 = abs(Mx - m)
+    D2 = abs(Dx - g)
+
+    # Заполнение таблицы
+    mytable.add_row([N, round(Mx, 4), round(m, 4), round(D1, 4), round(Dx, 4), round(g, 4), round(D2, 4)])
+
+
+for n in nums:
+    calculate(n)
 
 # Вывод таблицы
+print("Функция распределения: 3 / 14 * np.sqrt(x)")
 print(mytable)
